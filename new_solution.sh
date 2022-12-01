b6f8d571425527531e95ff96e71e62c74c08af60
yijiahe <yi.jiahe00@gmail.com>
2022-12-01 09:39:19 +0000

    add new solution script and templates

---
+++ new_solution.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+if [[ -z $1 ]]; then
+  echo "Please provide a day"
+  exit 1
+fi
+
+day=$1
+
+if [ -d "day_${day}" ]; then
+  echo "Solution already exists for day ${day}"
+  exit 1
+fi
+
+cp days/template_day.txt "days/day_${day}.py"
+touch "inputs/day_${day}_input.txt"
+cp tests/template_test.txt "tests/test_day_${day}.py"
