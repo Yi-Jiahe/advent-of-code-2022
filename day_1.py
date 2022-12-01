94efae290d22322e4a6860b9713535f088657e0d
yijiahe <yi.jiahe00@gmail.com>
2022-12-01 07:30:15 +0000

    Optimizations for part 1

--- day_1.py
+++ day_1.py
@@ -4,25 +4,18 @@ class Solution:
 
     def load_data(self):
         with open('day_1_input.txt') as f:
-            elf = []
+            calories = 0
             for line in map(lambda line: line.strip(), f):
                 if line == '':
-                    self.elves.append(elf)
-                    elf = []
+                    self.elves.append(calories)
+                    calories = 0
                 else:
-                    elf.append(int(line))
+                    calories += int(line)
 
-    def part_one(self):
-        # Part 1
-        elf_no, max_calories = None, 0
-        for i, elf in enumerate(self.elves):
-            calories = sum(elf)
-            if calories > max_calories:
-                elf_no = i
-                max_calories = calories
-        
-        print(f"Elf {elf_no} has the most calories at {max_calories}cal.")
+        self.elves.sort()
+
+    def part_one(self):    
+        print(f"The most calories carried by an elf is {self.elves[-1]}cal.")
 
     def part_two(self):
-        calories = sorted(map(sum, self.elves))
-        print(f"Total calories of top three elves is {sum(calories[-3:])}cal.")
+        print(f"Total calories of top three elves is {sum(self.elves[-3:])}cal.")
