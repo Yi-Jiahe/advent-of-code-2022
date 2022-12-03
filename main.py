179dfad2dcd3df240db3b50b05955476048af9af
yijiahe <yi.jiahe00@gmail.com>
2022-12-03 06:30:37 +0000

    make day an optional arguement

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import datetime
+import zoneinfo
+
+def get_curr_day():
+    now = datetime.datetime.now(zoneinfo.ZoneInfo("America/New_York"))
+    if now < datetime.datetime.fromisoformat("2022-12-01 00:00:00.000000-05:00"):
+        return "01"
+    elif now < datetime.datetime.fromisoformat("2022-12-25 23:59:59.999999-05:00"):
+        return now.strftime("%d")
+    else:
+        return "25"
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(
                     prog = 'Advent of Code 2022',
                     description = 'Provides solution to challenges')
-    parser.add_argument('day', type=int)
+    parser.add_argument('day', type=int, nargs='?', default=get_curr_day())
     parser.add_argument('--part', type=int)
     args = parser.parse_args()
 
