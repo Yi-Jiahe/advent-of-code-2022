605feb46c025fe31111bf04b10d7f63bc082db61
yijiahe <yi.jiahe00@gmail.com>
2022-12-20 07:35:18 +0000

    Correct merge misshap

--- days/day_20.py
+++ days/day_20.py
@@ -1,9 +1,29 @@
+class Solution:
+    def __init__(self):
+        pass
 
-    def part_two(self, data: []) -> str:
+    def load_from_file(self, filepath: str):
+        with open(filepath) as f:
+            return self.load(f)
+
+    def load(self, iterable) -> []:
+        ret = []
+        for line in map(lambda line: line.strip(), iterable):
+            ret.append(int(line))
+        return ret
+
+    def part_one(self, data: []) -> str:
         ans = None
+
+        numbered_data = [(i, entry) for i, entry in enumerate(data)]
+
+        for i, entry in enumerate(data):
+            print(numbered_data.index((i, entry)))
+
         raise NotImplementedError
         print(f"Ans: {ans}")
         return str(ans)
+
     def part_two(self, data: []) -> str:
         ans = None
         raise NotImplementedError
